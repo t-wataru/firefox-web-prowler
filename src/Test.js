12a93e7168be931a98ae89b030eed8df241ac030
wataru.tsujimoto <tsujimoto.wataru+git@g.wakayama-u.jp>
2021-02-21 23:06:16 +0900

    code format

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,13 @@
 class Test {
     static run() {
-        if (!this.ENABLE) { return }
+        if (!this.ENABLE) {
+            return;
+        }
         const keys = Object.keys(this);
         const tests = keys
-            .filter(key => key.includes("test"))
-            .map(key => this[key])
-            .filter(test => test.constructor == Function || test.constructor.constructor == Function)
+            .filter((key) => key.includes('test'))
+            .map((key) => this[key])
+            .filter((test) => test.constructor == Function || test.constructor.constructor == Function)
             .sort(Math.random);
         for (let test of tests) {
             test();
@@ -13,12 +15,12 @@ class Test {
     }
 }
 Test.ENABLE = false;
-Test.log = Test.ENABLE ? console.log.bind(null, "TEST:") : () => { };
-Test.assert = Test.ENABLE ? console.assert.bind(null) : () => { };
+Test.log = Test.ENABLE ? console.log.bind(null, 'TEST:') : () => {};
+Test.assert = Test.ENABLE ? console.assert.bind(null) : () => {};
 
 Test.test_テストクラスのテスト関数を失敗させるテスト = function () {
-    Test.assert(false, "これが失敗してエラーになれば、ちゃんとテストが実行されている");
+    Test.assert(false, 'これが失敗してエラーになれば、ちゃんとテストが実行されている');
 };
 Test.test_テストクラスのテスト関数を成功させるテスト = function () {
-    Test.assert(true, "これが失敗してエラーになれば、テスト関数がバグってる");
+    Test.assert(true, 'これが失敗してエラーになれば、テスト関数がバグってる');
 };
